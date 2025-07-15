11b7953798708f6a0f4b3bbfa5c029b260b6ed13
13th-Sammy <213174535+13th-Sammy@users.noreply.github.com>
2025-07-15 21:48:51 +0530

    Update

--- app/src/main/java/lazy/text/Main.java
+++ app/src/main/java/lazy/text/Main.java
@@ -1,12 +1,20 @@
 package lazy.text;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         try(Connection conn=DBConnection.getConnection()) {
-            System.out.println("Werks");
+            try(PreparedStatement ps=conn.prepareStatement("SELECT * FROM users")) {
+                try(ResultSet rs=ps.executeQuery()) {
+                    while(rs.next()) {
+                        System.out.println(rs.getString("username"));
+                    }
+                }
+            }
         } catch (SQLException | RuntimeException e) {
             System.err.println("Could not connect to Supabase: "+e.getMessage());
         }
